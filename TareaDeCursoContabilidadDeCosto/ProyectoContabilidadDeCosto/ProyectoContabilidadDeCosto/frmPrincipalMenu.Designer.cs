d1631187beaf4a27033a7aafd23389999daf268d
Stephanie Carolina Tenorio Orozco <stephanie.tenorio48u@std.uni.edu.ni>
2022-11-12 17:47:20 -0600

    Diseño de Form

---
+++ TareaDeCursoContabilidadDeCosto/ProyectoContabilidadDeCosto/ProyectoContabilidadDeCosto/frmPrincipalMenu.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace ProyectoContabilidadDeCosto
+{
+    partial class frmPrincipalMenu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmPrincipalMenu));
+            this.plMenu = new System.Windows.Forms.Panel();
+            this.btnEntrada = new System.Windows.Forms.Button();
+            this.btnSalida = new System.Windows.Forms.Button();
+            this.btnProductos = new System.Windows.Forms.Button();
+            this.btnInventario = new System.Windows.Forms.Button();
+            this.plMenu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // plMenu
+            // 
+            this.plMenu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(22)))), ((int)(((byte)(43)))), ((int)(((byte)(73)))));
+            this.plMenu.Controls.Add(this.btnInventario);
+            this.plMenu.Controls.Add(this.btnProductos);
+            this.plMenu.Controls.Add(this.btnSalida);
+            this.plMenu.Controls.Add(this.btnEntrada);
+            this.plMenu.Location = new System.Drawing.Point(0, 0);
+            this.plMenu.Name = "plMenu";
+            this.plMenu.Size = new System.Drawing.Size(172, 550);
+            this.plMenu.TabIndex = 0;
+            // 
+            // btnEntrada
+            // 
+            this.btnEntrada.FlatAppearance.BorderSize = 0;
+            this.btnEntrada.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnEntrada.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnEntrada.ForeColor = System.Drawing.Color.White;
+            this.btnEntrada.Image = global::ProyectoContabilidadDeCosto.Properties.Resources.Descargar;
+            this.btnEntrada.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnEntrada.Location = new System.Drawing.Point(3, 164);
+            this.btnEntrada.Name = "btnEntrada";
+            this.btnEntrada.Size = new System.Drawing.Size(169, 57);
+            this.btnEntrada.TabIndex = 1;
+            this.btnEntrada.Text = "ENTRADA";
+            this.btnEntrada.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnEntrada.UseVisualStyleBackColor = true;
+            // 
+            // btnSalida
+            // 
+            this.btnSalida.FlatAppearance.BorderSize = 0;
+            this.btnSalida.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSalida.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnSalida.ForeColor = System.Drawing.Color.White;
+            this.btnSalida.Image = global::ProyectoContabilidadDeCosto.Properties.Resources.Subir;
+            this.btnSalida.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnSalida.Location = new System.Drawing.Point(3, 223);
+            this.btnSalida.Name = "btnSalida";
+            this.btnSalida.Size = new System.Drawing.Size(169, 57);
+            this.btnSalida.TabIndex = 2;
+            this.btnSalida.Text = "SALIDA";
+            this.btnSalida.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnSalida.UseVisualStyleBackColor = true;
+            // 
+            // btnProductos
+            // 
+            this.btnProductos.FlatAppearance.BorderSize = 0;
+            this.btnProductos.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnProductos.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnProductos.ForeColor = System.Drawing.Color.White;
+            this.btnProductos.Image = global::ProyectoContabilidadDeCosto.Properties.Resources.Productos;
+            this.btnProductos.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnProductos.Location = new System.Drawing.Point(3, 284);
+            this.btnProductos.Name = "btnProductos";
+            this.btnProductos.Size = new System.Drawing.Size(169, 57);
+            this.btnProductos.TabIndex = 3;
+            this.btnProductos.Text = "PRODUCTOS";
+            this.btnProductos.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnProductos.UseVisualStyleBackColor = true;
+            // 
+            // btnInventario
+            // 
+            this.btnInventario.FlatAppearance.BorderSize = 0;
+            this.btnInventario.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnInventario.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnInventario.ForeColor = System.Drawing.Color.White;
+            this.btnInventario.Image = global::ProyectoContabilidadDeCosto.Properties.Resources.Almacenados;
+            this.btnInventario.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnInventario.Location = new System.Drawing.Point(3, 345);
+            this.btnInventario.Name = "btnInventario";
+            this.btnInventario.Size = new System.Drawing.Size(169, 57);
+            this.btnInventario.TabIndex = 4;
+            this.btnInventario.Text = "INVENTARIO";
+            this.btnInventario.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnInventario.UseVisualStyleBackColor = true;
+            // 
+            // frmPrincipalMenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SteelBlue;
+            this.ClientSize = new System.Drawing.Size(998, 545);
+            this.Controls.Add(this.plMenu);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmPrincipalMenu";
+            this.Text = "frmPrincipalMenu";
+            this.plMenu.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Panel plMenu;
+        private Button btnEntrada;
+        private Button btnSalida;
+        private Button btnProductos;
+        private Button btnInventario;
+    }
+}
